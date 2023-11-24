a7c893ae0378f25a3903b8ac13f68e9d1325f17d
jumayev018 <eshborijumayev11@gmail.com>
2023-11-23 20:07:33 -0800

    destructuring via rest elements

---
+++ script.js
@@ -0,0 +1,8 @@
+const removeFirstTwo = (list) => {
+  const [, , ...listed] = list;
+  return listed;
+};
+
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
+console.log(sourceWithoutFirstTwo);
